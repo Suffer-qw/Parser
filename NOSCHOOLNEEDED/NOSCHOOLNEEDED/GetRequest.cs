6017f3ddbdd24173417738a4e114394c75b1598d
Suffer-qw <stas.eryomchenko@gmail.com>
2025-01-17 01:19:48 +0700

    NOSCHOOLNEEDED

---
+++ NOSCHOOLNEEDED/NOSCHOOLNEEDED/GetRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOSCHOOLNEEDED
+{
+    public class GetRequest
+    {
+
+        HttpWebRequest _request;
+        string _url;
+
+        public string Response {  get; set; }
+
+        public GetRequest(string url)
+        {
+            _url = url;
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_url);
+            _request.Method = "Get";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+
+            
+        }
+
+    }
+}
---
+++ NOSCHOOLNEEDED/NOSCHOOLNEEDED/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NOSCHOOLNEEDED
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "shool";
+
+            var request = new GetRequest($"https://kitsu.io/api/edge/anime?filter[text]={text}");
+            request.Run();
+
+            var response = request.Response;
+
+            var json = JObject.Parse(response);
+
+            var data = json["data"];
+
+            foreach (var item in data)
+            {
+                var attri = item["attributes"];
+                var name = attri["slug"];
+                Console.WriteLine($"name = {name}\n");
+            }
+
+        }
+    }
+}
